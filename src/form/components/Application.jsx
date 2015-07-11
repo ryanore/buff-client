27db63fa660c7c26af99de4b61ce3a1fbf14b814
Ryan Ore <ryanore@RyanOre2015-MAC.local>
2015-07-11 15:45:33 -0500

    Add temp styles to test loader

---
+++ src/form/components/Application.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './temp.scss';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        Hello reacts!
+      </div>
+    );
+  }
+});
